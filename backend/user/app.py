49ccb987d729a85e379846328e849e56fa4e7f8d
Alina Shabalina <alinavshabalina@gmail.com>
2024-06-30 00:38:42 +0200

    add register, auth & login

--- backend/user/app.py
+++ backend/user/app.py
@@ -1,10 +1,12 @@
+import json
 import os
 
 from dotenv import load_dotenv
-from flask import Flask, jsonify
-from flask_login import LoginManager
-
+from flask import Flask, jsonify, request, flash
+from flask_login import LoginManager, current_user, login_user, login_required, logout_user
 from user import UserDB
+from flask_bcrypt import check_password_hash, generate_password_hash
+
 
 load_dotenv()
 app = Flask(__name__)
@@ -13,11 +15,59 @@ app.secret_key = os.getenv('FLASK_SECRET_KEY')
 login_manager.init_app(app)
 
 
-@app.route('/')
-def index():
-    data = UserDB().user_loader("1")
-    response = {
-        "message": f"User {data}",
+@app.route("/register", methods=["POST"])
+def register():
+    if current_user.is_authenticated:
+        flash("You are already registered")
+    else:
+        try:
+            password = json.loads(request.data)['password']
+            email = json.loads(request.data)['email']
+            password_hash = generate_password_hash(password, 10)
+            user = UserDB().add_user(email, password_hash)
+            if user:
+                login_user(user)
+                flash("You registered and are now logged in. Welcome!", "success")
+            response = {"message": True}
+            return jsonify(response), 200
+        except Exception as e:
+            return e.args
 
-    }
+
+@app.route('/login', methods=['POST'])
+def login():
+    error = None
+    if current_user.is_authenticated:
+        flash("You are already logged in.")
+    else:
+        try:
+            if request.method == 'POST':
+                email = request.form['email']
+                password = request.form['password']
+                user = UserDB().find_user(email)
+                if user and check_password_hash(user[3], password):
+                    login_user(user)
+                    if login_user(user):
+                        response = {"message": user.email}
+        except Exception as e:
+            return e.args
+        return jsonify(response), 200
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return UserDB.find_user_by_id(int(user_id))
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    response = {"message": True}
+    return jsonify(response), 200
+
+
+@app.route('/', methods=['GET'])
+def index():
+    response = {"message": True}
     return jsonify(response), 200
--- backend/user/user.py
+++ backend/user/user.py
@@ -1,27 +1,38 @@
-import flask_login
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin
 
 from init_db import DataBase
 
 login_manager = LoginManager()
 
 
-class UserDB(DataBase, flask_login.UserMixin):
+class UserDB(DataBase, UserMixin):
 
     def __init__(self):
         super().__init__()
         self.cur.execute('DROP TABLE IF EXISTS users;')
         self.cur.execute(
             '''CREATE TABLE IF NOT EXISTS users (id serial \
-        PRIMARY KEY, username varchar(150), email varchar(256), password_hash varchar(60), is_admin boolean);''')
-        self.cur.execute('INSERT INTO users (username, email, is_admin)'
-                         'VALUES (%s, %s, %s)',
-                         ('admin',
-                          'admin@maat_zine.com', True)
-                         )
+        PRIMARY KEY, username varchar(150), email varchar(256), password_hash varchar, is_admin boolean);''')
         self.conn.commit()
 
-    @login_manager.user_loader
-    def user_loader(self, user_id):
-        self.cur.execute(f"SELECT * FROM users WHERE id = '{user_id}'")
-        return bool(self.cur.fetchone())
+    def add_user(self, email, password):
+        self.cur.execute('INSERT INTO users (username, email, password_hash, is_admin)'
+                         'VALUES (%s, %s, %s, %s)',
+                         ('',
+                          email, password, False))
+        self.conn.commit()
+        self.cur.execute(f"SELECT * FROM users WHERE users.email = '{email}'")
+        return self.cur.fetchone()[1]
+
+    def find_user(self, email):
+        self.cur.execute(f"SELECT * FROM users WHERE users.email = '{email}'")
+        return self.cur.fetchone()
+
+    def find_user_by_id(self, user_id):
+        self.cur.execute(f"SELECT * FROM users WHERE users.id = {user_id}")
+        return self.cur.fetchone()
+
+
+
+
+
