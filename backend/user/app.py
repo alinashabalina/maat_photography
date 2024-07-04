ba004e8c6bfd937540840cbb5cba726c86f2a784
Alina Shabalina <alinavshabalina@gmail.com>
2024-07-04 14:46:40 +0200

    fix register, auth & login

--- backend/user/app.py
+++ backend/user/app.py
@@ -3,10 +3,10 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request, flash
-from flask_login import LoginManager, current_user, login_user, login_required, logout_user
-from user import UserDB
 from flask_bcrypt import check_password_hash, generate_password_hash
+from flask_login import LoginManager, current_user, login_user, login_required, logout_user
 
+from user import UserDB
 
 load_dotenv()
 app = Flask(__name__)
@@ -15,56 +15,68 @@ app.secret_key = os.getenv('FLASK_SECRET_KEY')
 login_manager.init_app(app)
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return UserDB().find_user_by_id(int(user_id))
+
+
 @app.route("/register", methods=["POST"])
 def register():
-    if current_user.is_authenticated:
-        flash("You are already registered")
-    else:
-        try:
+    try:
+        if current_user.is_authenticated:
+            flash("You are already registered")
+        else:
             password = json.loads(request.data)['password']
             email = json.loads(request.data)['email']
-            password_hash = generate_password_hash(password, 10)
+            password_hash = generate_password_hash(password, 10).decode('utf8')
             user = UserDB().add_user(email, password_hash)
             if user:
-                login_user(user)
-                flash("You registered and are now logged in. Welcome!", "success")
-            response = {"message": True}
-            return jsonify(response), 200
-        except Exception as e:
-            return e.args
+                response = {"message": "You have successfully registered"}
+                return jsonify(response), 200
+            else:
+                response = {"message": "You are not registered. Please try again later"}
+                return jsonify(response), 400
+    except Exception as e:
+        return e.args
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    error = None
-    if current_user.is_authenticated:
-        flash("You are already logged in.")
-    else:
-        try:
+    try:
+        if current_user.is_authenticated:
+            response = {"message": current_user.email}
+            return jsonify(response), 200
+        else:
             if request.method == 'POST':
                 email = request.form['email']
                 password = request.form['password']
                 user = UserDB().find_user(email)
-                if user and check_password_hash(user[3], password):
-                    login_user(user)
+                if user and check_password_hash(user.password_hash, password):
                     if login_user(user):
-                        response = {"message": user.email}
-        except Exception as e:
-            return e.args
-        return jsonify(response), 200
+                        response = {"message": "You are successfully logged in"}
+                        return jsonify(response), 200
+                    else:
+                        response = {"message": "Try again later"}
+                        return jsonify(response), 400
+                else:
+                    response = {"message": "Either your email or your password are incorrect. Please try again"}
+                    return jsonify(response), 400
+    except Exception as e:
+        response = {"message": e.args}
+        return jsonify(response), 400
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return UserDB.find_user_by_id(int(user_id))
-
-
-@app.route('/logout')
+@app.route('/logout', methods=['POST'])
 @login_required
 def logout():
-    logout_user()
-    response = {"message": True}
-    return jsonify(response), 200
+    try:
+        if current_user.is_authenticated:
+            logout_user()
+            response = {"message": "You are logged out"}
+            return jsonify(response), 200
+    except Exception as e:
+        response = {"message": e.args}
+        return jsonify(response), 400
 
 
 @app.route('/', methods=['GET'])
--- backend/user/user.py
+++ backend/user/user.py
@@ -5,11 +5,24 @@ from init_db import DataBase
 login_manager = LoginManager()
 
 
-class UserDB(DataBase, UserMixin):
+@login_manager.user_loader
+def load_user(user_id):
+    return UserDB().find_user_by_id(int(user_id))[0]
+
+
+class User(UserMixin):
+    def __init__(self, user_id, username, email, password_hash, is_admin):
+        self.id = user_id
+        self.username = username
+        self.email = email
+        self.password_hash = password_hash
+        self.is_admin = is_admin
+
+
+class UserDB(DataBase):
 
     def __init__(self):
         super().__init__()
-        self.cur.execute('DROP TABLE IF EXISTS users;')
         self.cur.execute(
             '''CREATE TABLE IF NOT EXISTS users (id serial \
         PRIMARY KEY, username varchar(150), email varchar(256), password_hash varchar, is_admin boolean);''')
@@ -22,17 +35,16 @@ class UserDB(DataBase, UserMixin):
                           email, password, False))
         self.conn.commit()
         self.cur.execute(f"SELECT * FROM users WHERE users.email = '{email}'")
-        return self.cur.fetchone()[1]
+        return self.cur.fetchone()
 
     def find_user(self, email):
         self.cur.execute(f"SELECT * FROM users WHERE users.email = '{email}'")
-        return self.cur.fetchone()
+        data = self.cur.fetchone()
+        user = User(data[0], data[1], data[2], data[3], data[4])
+        return user
 
     def find_user_by_id(self, user_id):
         self.cur.execute(f"SELECT * FROM users WHERE users.id = {user_id}")
-        return self.cur.fetchone()
-
-
-
-
-
+        data = self.cur.fetchone()
+        user = User(data[0], data[1], data[2], data[3], data[4])
+        return user
