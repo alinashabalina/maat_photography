31cf6fcd52fa986846d24cdbd1a7bb99d769f1fd
Alina Shabalina <alinavshabalina@gmail.com>
2024-08-09 14:32:56 +0200

    fix personal area undefined problem

--- ma'at_photography/src/stores/UserStore.js
+++ ma'at_photography/src/stores/UserStore.js
@@ -3,22 +3,18 @@ export const UserStore = defineStore('user', {
     state: () => ({
         user: []
     }),
-    getters: {
-        getData() {
-            return (user_id) =>
-            this.getUserData(user_id)
 
-        },
+    getters: {
         getDetails(state) {
             return state.user
         }
-
     },
+
     actions: {
         getUserData(user_id) {
             fetch(` http://127.0.0.1:5000/user/${user_id}` )
                 .then((response) => response.json())
-                .then((data) => this.user.push(data["user_info"]))
+                .then((data) => this.user.push(data.user_info))
         },
     },
 })
--- ma'at_photography/src/views/PersonalAreaView.vue
+++ ma'at_photography/src/views/PersonalAreaView.vue
@@ -1,27 +1,35 @@
 <script setup lang="ts">
-import {ref} from "vue"
+import {ref, toRaw} from "vue"
 import HeaderComponent from "@/components/HeaderComponent.vue";
 import PACardComponent from "@/components/PACardComponent.vue";
 import { UserStore } from '@/stores/UserStore'
 const favorites = ref("favorite pictures")
 const reading = ref("reading list")
 const orders = ref("orders")
+const favs = ref([])
+const reads = ref([])
+const ords = ref([])
 const store = UserStore()
-store.getData(27)
-const details = store.getDetails[0]
+store.getUserData(26)
+const details = toRaw(store.getDetails)
+if (details !== undefined && details[0] !== undefined) {
+  favs.value = details[0].user_favorites
+  reads.value = details[0].user_reads
+  ords.value = details[0].user_orders
+}
 </script>
 
 <template>
   <HeaderComponent/>
   <div class="card-wrapper">
     <PACardComponent class="card" :header="favorites">
-      <div v-for="item in details.user_favorites" :key="item"> {{ item }} </div>
+      <div v-for="item in favs" :key="item"> {{ item }} </div>
     </PACardComponent>
     <PACardComponent class="card" :header="reading">
-      <div v-for="item in details.user_reads" :key="item"> {{ item }} </div>
+      <div v-for="item in reads" :key="item"> {{ item }} </div>
     </PACardComponent>
     <PACardComponent class="card"  :header="orders">
-      <div v-for="item in details.user_orders" :key="item"> {{ item }} </div>
+      <div v-for="item in ords" :key="item"> {{ item }} </div>
     </PACardComponent>
   </div>
 
